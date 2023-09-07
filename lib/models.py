77e08311af7cf8c004a9ad2b25ba872ee8f1a2c8
Martin Nyaga <martin.nyaga@student.moringaschool.com>
2023-09-07 13:16:25 +0300

    added click lib

--- lib/models.py
+++ lib/models.py
@@ -1,9 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String,Date,ForeignKey
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///hospital_database.db')
+
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
 
 Base = declarative_base()
 
